03b311699c407c7c6d495ba1612a3d6f9ecd2dd9
Mario Fernández <nilovinilo1@gmail.com>
2024-01-30 23:13:47 +0100

    State Managen Single Page To all Nav Links

--- pages/Components/Hero.tsx
+++ pages/Components/Hero.tsx
@@ -16,47 +16,52 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center">
-      <Particle />
-      <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
-        <div className="">
-          <h1 className="text-[35px] md:text-[50px] text-white font-bold ">
-            HI, I'M <samp className="text-yellow-400">Mario</samp>
-          </h1>
-          <TextEffect />
-          <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
-            sed et dolorum? Cupiditate, odio. Qui nobis, asperiores sit mollitia
-            amet sequi eius eaque laudantium quos.
-          </p>
-          <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
-            <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-cemter space-x-2">
-              <p>Download CV</p>
-              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
-            </button>
-            <button className="flex items-center space-x-2">
-              <PlayCircleIcon className="w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200 text-[#55e6a5] " />
-              <p className="text-[20px] font-semibold text-white">
-                Watch The Video
-              </p>
-            </button>
+    <section id="home">
+      <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center">
+        <Particle />
+        <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+          <div className="">
+            <h1 className="text-[35px] md:text-[50px] text-white font-bold ">
+              HI, I'M <samp className="text-yellow-400">Mario</samp>
+            </h1>
+            <TextEffect />
+            <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Asperiores sed et dolorum? Cupiditate, odio. Qui nobis, asperiores
+              sit mollitia amet sequi eius eaque laudantium quos.
+            </p>
+            <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+              <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-cemter space-x-2">
+                <p>Download CV</p>
+                <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+              </button>
+              <button className="flex items-center space-x-2">
+                <PlayCircleIcon className="w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200 text-[#55e6a5] " />
+                <p className="text-[20px] font-semibold text-white">
+                  Watch The Video
+                </p>
+              </button>
+            </div>
+          </div>
+          <div
+            data-aos="zoom-in"
+            className="w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]"
+          >
+            <Image
+              src="/images/mdfv.jpg"
+              alt="user"
+              layout="fill"
+              className="object-cover rounded-full"
+              style={{
+                objectPosition: "center top",
+                transform: "scale(.99993)",
+              }}
+              unoptimized
+            />
           </div>
         </div>
-        <div
-          data-aos="zoom-in"
-          className="w-[500px] bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]"
-        >
-          <Image
-            src="/images/mdfv.jpg"
-            alt="user"
-            layout="fill"
-            className="object-cover rounded-full"
-            style={{ objectPosition: "center top", transform: "scale(.99993)" }}
-            unoptimized
-          />
-        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
