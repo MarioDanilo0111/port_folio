a48e341e4e11139c2905820bd9863383af18d567
Mario Fernández <nilovinilo1@gmail.com>
2024-02-20 10:03:53 +0100

    Fix build errors, Images unoptimized

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  images: {
+    unoptimized: true, // Disable Next.js Image Optimization API for local images
+  },
 };
 
 module.exports = nextConfig;
