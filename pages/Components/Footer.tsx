60e565af62ca59dd416ed018f8c5b9caeff4db66
Mario Fernández <nilovinilo1@gmail.com>
2024-03-14 12:25:30 +0100

    Adjusting Footer Rendering

--- pages/Components/Footer.tsx
+++ pages/Components/Footer.tsx
@@ -8,6 +8,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className=" pt-[3rem] pb-[4rem] bg-[#02050a] ">
+      {/* 
       <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] ">
         <div className="flex items-center space-x-6">
           <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5] ">
@@ -60,6 +61,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
+        */}
       <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between ">
         <div className=" text-[18px] md-[2rem] md:mb-0 text-white opacity-20 ">
           niloDev ninja 2024 | all Rights Reserved
