63bb61b090c3742323ce2af5a7197fd8421848ec
Mario Fernández <nilovinilo1@gmail.com>
2024-03-17 10:41:14 +0100

    Following tailwind Image idications adjusting tailwind config file mobile L

--- pages/Components/Projects.tsx
+++ pages/Components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/legacy/image";
+import ImageNext from "next/image";
 
 const Projects = () => {
   return (
@@ -23,34 +24,46 @@ const Projects = () => {
                 update, and delete notes. Security and user authentication are
                 bolstered by Passport and its Google OAuth2.0 strategy.
               </p>
-              <div className="flex items-start gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[.6rem]">
-                <img
-                  src="/icons/expressjs.webp"
-                  alt="expressJs"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/mongodb-icon.svg"
-                  alt="mongoDB"
-                  className="w-[6%] md:w-[3%] h-auto"
-                />
-                <img
-                  src="/icons/API.webp"
-                  alt="api"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/passport.svg"
-                  alt="passport"
-                  className="w-[6%] md:w-[5%] h-auto"
-                />
+              <div className="flex items-start gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[1.3rem]">
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/expressjs.webp"
+                    alt="expressJs"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[4%] md:w-[6%] lg:w-[2%]">
+                  <ImageNext
+                    src="/icons/mongodb-icon.svg"
+                    alt="mongoDB"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/API.webp"
+                    alt="api"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[2%]">
+                  <ImageNext
+                    src="/icons/passport.svg"
+                    alt="passport"
+                    width={300}
+                    height={300}
+                  />
+                </div>
               </div>
               <div className="transform cursor-pointer hover:rotate-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
                   src="/images/notesApp.webp"
                   alt="portfolio"
-                  layout="fill"
-                  objectFit="contain"
+                  width={500}
+                  height={300}
                 />
               </div>
             </a>
@@ -67,35 +80,47 @@ const Projects = () => {
                 This app is a web application built with React, utilizing
                 various dependencies for front-end design and functionality.
               </p>
-              <div className="flex items-start gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[.6rem]">
-                <img
-                  src="/icons/react.svg"
-                  alt="reactJs"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/react-router.webp"
-                  alt="react-router"
-                  className="w-[16%] md:w-[9%] h-auto"
-                />
-                <img
-                  src="/icons/javascript.webp"
-                  alt="js"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/bootstrap.webp"
-                  alt="bootstrap"
-                  className="w-[9%] md:w-[6%] h-auto"
-                />
+              <div className="flex items-start gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[1.3rem]">
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/react.svg"
+                    alt="reactJs"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/react-router.webp"
+                    alt="react-router"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/javascript.webp"
+                    alt="js"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/bootstrap.webp"
+                    alt="bootstrap"
+                    width={300}
+                    height={300}
+                  />
+                </div>
               </div>
 
               <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
                   src="/images/50pro.webp"
                   alt="portfolio"
-                  layout="fill"
-                  objectFit="contain"
+                  width={500}
+                  height={300}
                 />
               </div>
             </a>
@@ -114,35 +139,47 @@ const Projects = () => {
                 that will be delivered to the provided address.
               </p>
 
-              <div className="flex items-center gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[.6rem]">
-                <img
-                  src="/icons/vitejs.svg"
-                  alt="js"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/axios.webp"
-                  alt="axios"
-                  className="w-[6%] md:w-[13%] h-auto"
-                />
-                <img
-                  src="/icons/react-hooks.webp"
-                  alt="rect-hooks"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/typescript-icon.webp"
-                  alt="tys"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
+              <div className="flex items-center gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[1.3rem]">
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/vitejs.svg"
+                    alt="js"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/axios.webp"
+                    alt="axios"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/react-hooks.webp"
+                    alt="rect-hooks"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/typescript-icon.webp"
+                    alt="tys"
+                    width={300}
+                    height={300}
+                  />
+                </div>
               </div>
 
               <div className="transform cursor-pointer hover:-rotate-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
                   src="/images/candyAxios.webp"
                   alt="portfolio"
-                  layout="fill"
-                  objectFit="contain"
+                  width={500}
+                  height={300}
                 />
               </div>
             </a>
@@ -160,34 +197,46 @@ const Projects = () => {
                 years, and band names to the data structure. With a full CRUD
                 functionality for interacting, utilizing Node's JSON server.
               </p>
-              <div className="flex items-center gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[.6rem]">
-                <img
-                  src="/icons/react.svg"
-                  alt=""
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/nodejs-plain.webp"
-                  alt="nodeJs"
-                  className="w-[6%] md:w-[4rem] h-auto"
-                />
-                <img
-                  src="/icons/crud.webp"
-                  alt="crud"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/typeJson.webp"
-                  alt=""
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
+              <div className="flex items-center gap-[1rem] overflow-auto mb-[1.3rem] md:mb-[1.3rem]">
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/react.svg"
+                    alt="react"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/nodejs-plain.webp"
+                    alt="nodeJs"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/crud.webp"
+                    alt="crud"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/typeJson.webp"
+                    alt="typeJson"
+                    width={300}
+                    height={300}
+                  />
+                </div>
               </div>
               <div className="transform cursor-pointer hover:-rotate-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
                   src="/images/save-miw.webp"
                   alt="portfolio"
-                  layout="fill"
-                  objectFit="contain"
+                  width={500}
+                  height={300}
                 />
               </div>
             </a>
@@ -205,34 +254,46 @@ const Projects = () => {
                 from the request parameters and efficiently organized using
                 TypeScript for typing support.
               </p>
-              <div className="flex items-center gap-[1rem] overflow-auto mb-[1rem] md:mb-[.4rem]">
-                <img
-                  src="/icons/vitejs.svg"
-                  alt=""
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/typescript-icon.webp"
-                  alt=""
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/axios.webp"
-                  alt="axios"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/node-sass.webp"
-                  alt="nodeJs"
-                  className="w-[6%] md:w-[4rem] h-auto"
-                />
+              <div className="flex items-center gap-[1rem] overflow-auto mb-[1rem] md:mb-[1.3rem]">
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/vitejs.svg"
+                    alt="viteJs"
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/typescript-icon.webp"
+                    alt="tys"
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/axios.webp"
+                    alt="axios"
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/node-sass.webp"
+                    alt="nodeJs"
+                    height={300}
+                    width={300}
+                  />
+                </div>
               </div>
               <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
                   src="/images/hackerNews.webp"
                   alt="portfolio"
-                  layout="fill"
-                  objectFit="contain"
+                  width={500}
+                  height={300}
                 />
               </div>
             </a>
@@ -257,34 +318,46 @@ const Projects = () => {
                 project was a significant milestone in my coding journey
                 developing with React.
               </p>
-              <div className="flex items-center gap-[1rem] overflow-auto mb-[1rem] md:mb-[.3rem]">
-                <img
-                  src="/icons/react.svg"
-                  alt=""
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/emailJs.webp"
-                  alt="emailJs"
-                  className="w-[3%] md:w-[3rem] h-auto"
-                />
-                <img
-                  src="/icons/bootstrap.webp"
-                  alt="crud"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
-                <img
-                  src="/icons/react-router.webp"
-                  alt="reactRouter"
-                  className="w-[6%] md:w-[6%] h-auto"
-                />
+              <div className="flex items-center gap-[1rem] overflow-auto mb-[1rem] md:mb-[1.3rem]">
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/react.svg"
+                    alt="reactJs"
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/emailJs.webp"
+                    alt="emailJs"
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/bootstrap.webp"
+                    alt="crud"
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className="w-full ml:w-[5%] md:w-[6%] lg:w-[3%]">
+                  <ImageNext
+                    src="/icons/react-router.webp"
+                    alt="reactRouter"
+                    height={300}
+                    width={300}
+                  />
+                </div>
               </div>
               <div className="transform cursor-pointer hover:rotate-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
                   src="/images/beginersPortfolio.webp"
                   alt="portfolio"
-                  layout="fill"
-                  objectFit="contain"
+                  width={500}
+                  height={300}
                 />
               </div>
             </a>
